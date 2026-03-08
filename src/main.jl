41b259520f3ad29b665570511b3c4a3d17c838fe
laslo <laslo.hauschild@web.de>
2026-03-08 18:49:28 +0100

    initial commit

---
+++ src/main.jl
@@ -0,0 +1,136 @@
+using Graphs
+using Distances
+using Zygote
+using Plots
+using PlotThemes
+using LinearAlgebra
+
+function plot_graph(g::SimpleGraph, p::Matrix{Float64})
+    plt = plot(legend = false, aspect_ratio = :equal)
+    for e in edges(g)
+        plot!(plt, [p[1, src(e)], p[1, dst(e)]], [p[2, src(e)], p[2, dst(e)]],
+                color = :black)
+    end
+    scatter!(plt, p[1, :], p[2, :], markersize=5, color = :blue)
+    return plt
+end
+
+function stress(
+    g::SimpleGraph, 
+    p::Matrix{Float64}, 
+    edge_stress_f::Function, 
+    vertex_stress_f::Function, 
+    combine_f::Function)
+
+    n = nv(g)
+    edge_stress = 0
+    for e in edges(g)
+        edge_stress += edge_stress_f(src(e), dst(e))
+    end
+    vertex_stress = 0
+    for i in 1:n
+        for j in (i+1):n
+            vertex_stress += vertex_stress_f(i, j)
+        end
+    end
+
+    return combine_f(edge_stress, vertex_stress)
+end
+
+function fr_stress(g::SimpleGraph, p::Matrix{Float64})
+    D = pairwise(Euclidean(), p, dims=2)
+    edge_stress_f = (i, j) -> D[i, j]^3
+    vertex_stress_f = (i, j) -> log(D[i, j] + 10^(-6))
+    combine_f = (x, y) -> x / 6 - y
+    return stress(g, p, edge_stress_f, vertex_stress_f, combine_f)
+end
+
+
+function erll_stress(g::SimpleGraph, p::Matrix{Float64}, deg::Vector{Int})
+    D = pairwise(Euclidean(), p, dims=2)
+    edge_stress_f = (i, j) -> D[i, j]
+    vertex_stress_f = (i, j) -> deg[i] * deg[j] * log(D[i, j] + 10^(-6))
+    combine_f = (x, y) -> x / 2 - y
+    return stress(g, p, edge_stress_f, vertex_stress_f, combine_f)
+end
+
+function simple_gradient_descent(g::SimpleGraph, alpha::Float64, max_steps::Int64)
+    n = nv(g) # Number of vertecies
+    deg = degree(g) # The degrees of the edges
+    p = rand(2, n) # Initial positions
+
+    stresses = Float64[]
+
+    for k in 1:max_steps
+        s, (grad,) = Zygote.withgradient(q -> fr_stress(g, q), p)
+        push!(stresses, s)
+
+        if length(stresses) > 1 && abs(stresses[end] - stresses[end-1]) < 1e-6
+            println("converged!")
+            plot1 = plot_graph(g, p)
+            plot2 = plot(stresses, label="stress")
+            final = plot(plot1, plot2, layout=2)
+            savefig(final, "converged.svg")
+            break
+        end
+
+        p -= alpha * grad
+
+        if k % 30 == 0
+            plot1 = plot_graph(g, p)
+            plot2 = plot(stresses, label="stress")
+            display(plot(plot1, plot2, layout=2))
+        end
+    end
+end
+
+function nesterov_gradient_descent(g::SimpleGraph, alpha::Float64, beta::Float64, max_steps::Int64)
+    n = nv(g) # Number of vertecies
+    deg = degree(g) # The degrees of the edges
+    p = rand(2, n) # Initial positions
+
+    stresses = Float64[]
+
+    momentum = zeros(2, n)
+
+    for k in 0:max_steps
+        s, (grad,) = Zygote.withgradient(q -> fr_stress(g, q), p + beta * momentum)
+        new_momentum = beta * momentum - alpha * grad
+        #println("k=$k  s=$s  norm(p)=$(norm(p))  norm(grad)=$(norm(grad))")
+        push!(stresses, s)
+
+        if length(stresses) > 50 && abs(stresses[end] - stresses[end-1]) < 1e-9
+            println("converged!")
+            println("last stress: ", stresses[end])
+            println("previous stress: ", stresses[end - 1])
+            println("iteration ", k)
+            plot1 = plot_graph(g, p)
+            plot2 = plot(stresses, label="stress")
+            final = plot(plot1, plot2, layout=2)
+            savefig(final, "converged.svg")
+            break
+        end
+
+        p += new_momentum
+        momentum = new_momentum
+
+        if k % 30 == 0
+            plot1 = plot_graph(g, p)
+            plot2 = plot(stresses, label="stress")
+            display(plot(plot1, plot2, layout=2))
+        end
+    end
+end
+
+function run(g)
+    nesterov_gradient_descent(g, 0.01, 0.9, 1000000)
+end
+
+g = SimpleGraph(8)
+for i in 0:7, j in i+1:7
+    if count_ones(i ⊻ j) == 1   # XOR: differ in exactly one bit
+        add_edge!(g, i+1, j+1)
+    end
+end
+
+run(g)
